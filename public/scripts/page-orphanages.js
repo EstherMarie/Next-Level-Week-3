555487ef0a409fc37819cf550b91f9f6c00bf4bf
EstherMarie <24815585+EstherMarie@users.noreply.github.com>
2020-10-18 20:33:13 -0300

    dia 4 concluído

--- public/scripts/page-orphanages.js
+++ public/scripts/page-orphanages.js
@@ -1,31 +1,43 @@
 // Create map
-const map = L.map('mapid').setView([-22.890227,-43.3045401], 12);
+const map = L.map('mapid').setView([-22.890227, -43.3045401], 12);
 
 // Create and add tileLayer | -22.8087/-43.2248
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution:
-        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+	attribution:
+		'&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
 
 // Create icon
 const icon = L.icon({
-    iconUrl: "./public/images/map-marker.svg",
-    iconSize: [58, 68],
-    iconAnchor: [29, 68],
-    popupAnchor: [170, 2]
+	iconUrl: '/images/map-marker.svg',
+	iconSize: [58, 68],
+	iconAnchor: [29, 68],
+	popupAnchor: [170, 2],
 });
 
-// create popup overlay 
-const popup = L.popup({
-    closeButton: false,
-    className: 'map-popup',
-    minWidth: 240,
-    minHeight: 240
-}).setContent(
-    'Lar da Felicidade <a href="orphanage.html?id=1" class="choose-orphanage"> <img src="./public/images/arrow-white.svg"> </a>'
-);
+function addMarker({ id, name, lat, lng }) {
+	// create popup overlay
+	const popup = L.popup({
+		closeButton: false,
+		className: 'map-popup',
+		minWidth: 240,
+		minHeight: 240,
+	}).setContent(
+		`${name} <a href="/orphanage?id=${id}" class="choose-orphanage"> <img src="/images/arrow-white.svg"> </a>`
+	);
 
-// Create icon
-L.marker([-22.890227,-43.3045401], { icon: icon })
-    .addTo(map)
-    .bindPopup(popup)
+	// Create icon
+	L.marker([lat, lng], { icon: icon }).addTo(map).bindPopup(popup);
+}
+
+const orphanagesSpan = document.querySelectorAll('.orphanages span');
+orphanagesSpan.forEach((span) => {
+	const orphanage = {
+		id: span.dataset.id,
+		name: span.dataset.name,
+		lat: span.dataset.lat,
+		lng: span.dataset.lng,
+	};
+
+	addMarker(orphanage);
+});
